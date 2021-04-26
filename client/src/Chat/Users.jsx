ac81c6b38799b209cb2f86916ed4f296d2d0e786
Andrii Zdrobylko <azdrobylko@KVL1910007.lohika.com>
2021-04-26 21:56:02 +0300

    add same part of address in avatar in list of users

--- client/src/Chat/Users.jsx
+++ client/src/Chat/Users.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import { makeStyles } from "@material-ui/core/styles";
-import socketIOClient from "socket.io-client";
+import React, { useState, useEffect } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import socketIOClient from 'socket.io-client';
 
-import { useGetUsers } from "../Services/userService";
-import commonUtilites from "../Utilities/common";
+import { useGetUsers } from '../Services/userService';
+import commonUtilites from '../Utilities/common';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   subheader: {
-    display: "flex",
-    alignItems: "center",
-    cursor: "pointer",
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
   },
   globe: {
     backgroundColor: theme.palette.primary.dark,
@@ -23,27 +23,27 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.primary.dark,
   },
   list: {
-    maxHeight: "calc(100vh - 112px)",
-    overflowY: "auto",
+    maxHeight: 'calc(100vh - 112px)',
+    overflowY: 'auto',
   },
   avatar: {
     margin: theme.spacing(0, 3, 0, 1),
   },
 }));
 
-const Users = (props) => {
+const Users = props => {
   const classes = useStyles();
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState(null);
   const getUsers = useGetUsers();
 
   useEffect(() => {
-    getUsers().then((res) => setUsers(res));
+    getUsers().then(res => setUsers(res));
   }, [newUser]);
 
   useEffect(() => {
     const socket = socketIOClient(process.env.REACT_APP_API_URL);
-    socket.on("users", (data) => {
+    socket.on('users', data => {
       setNewUser(data);
     });
   }, []);
@@ -54,7 +54,7 @@ const Users = (props) => {
     <List className={classes.list}>
       {users && (
         <React.Fragment>
-          {users.map((u) => (
+          {users.map(u => (
             <ListItem
               className={classes.listItem}
               key={u._id}
@@ -65,7 +65,7 @@ const Users = (props) => {
               button
             >
               <ListItemAvatar className={classes.avatar}>
-                <Avatar>{commonUtilites.getInitialsFromName(u.name)}</Avatar>
+                <Avatar>{commonUtilites.getLastChars(u?.name)}</Avatar>
               </ListItemAvatar>
               <ListItemText primary={u.name} />
             </ListItem>
